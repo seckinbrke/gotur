dad08ab461e626ce70f00a34f0a18ade50590d66
seckinbrke <seckin_brke@hotmail.com>
2019-12-02 23:51:32 +0300

    Delete cors

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-var cors = require('cors')
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
