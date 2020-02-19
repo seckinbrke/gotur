4dad8cb1d877e7c51810e1eb55c27cd1267ea8e9
seckinbrke <seckin_brke@hotmail.com>
2020-02-20 00:53:28 +0300

    Socket cancel

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ var userRouter = require('./routes/order');
 var app = express();
 const db = require('./helper/db')();
 var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
-var port = 5000;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,25 +31,7 @@ app.use('/mainType', mainTypeRouter);
 app.use('/enroll', enrollRouter);
 app.use('/user', userRouter);
 app.use('/order', orderRouter);
-io.on('connection', function (socket) {
-  socket.emit('userId', { userId: socket.id });
-  socket.on('messages', function (data) {
-    let messageObj = {
-      _id: new Date().getTime() + '#' + socket.id,
-      text: data.text,
-      createdAt: new Date(),
-      user: {
-        _id: socket.id,
-        name: data.username,
-        avatar: 'https://facebook.github.io/react/img/logo_og.png',
-      }
-    };
-    console.log('messageObj => ', messageObj);
-    socket.broadcast.emit('messages', { 'messages': messageObj });
-    socket.emit('messages', { 'messages': messageObj });
-  });
-});
-server.listen(port, () => console.log("server running on port : " + port))
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
